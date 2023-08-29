9d2772251ddaa284bba50b934c84fb7d2eb1a4d7
HolgadoJairoDavid <jairodavidholgado@gmail.com>
2023-08-29 05:46:31 -0300

    The last commit

--- bd/common.go
+++ bd/common.go
@@ -49,3 +49,33 @@ func ConnStr(clave models.SecretRDSJson) string {
 	fmt.Println(dsn)
 	return dsn
 }
+
+func UserIsAdmin(userUUID string) (bool, string) {
+	fmt.Println("Comienza userIsAdmin")
+
+	err := DbConnect()
+	if err != nil {
+		return false, err.Error()
+	}
+
+	defer Db.Close()
+
+	sentencia := "SELECT 1 FROM users WHERE User_UUID='" + userUUID + "' AND User_Status = 0"
+
+	rows, err := Db.Query(sentencia)
+
+	if err != nil {
+		return false, err.Error()
+	}
+	var valor string
+
+	rows.Next()
+	rows.Scan(&valor)
+
+	fmt.Println("UserIsAdmin > Ejecución exitosa - valor devuelto " + valor)
+	if valor == "1" {
+		return true, ""
+	}
+
+	return false, "User is not Admin"
+}
